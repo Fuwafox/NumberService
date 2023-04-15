4d98cc5da8e58f457a2cceb5b351472cf6d5373c
Fuwafox <valerikaist@gmail.com>
2023-04-15 18:27:27 +0900

    Initial commit

---
+++ NumberService/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NumberService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+
+        static long x;
+        
+        [HttpGet]
+        public long Get( long num) 
+        {
+            Thread thread = new Thread(() => { x = num; });
+            thread.Start();
+            return x;
+        }
+
+
+    }
+}
