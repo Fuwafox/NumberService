cd174d677da381957ed1e516380bead8f7f7a2af
Fuwafox <valerikaist@gmail.com>
2023-04-16 23:41:57 +0900

    branch_2
    добавлен сервис

--- NumberService/Program.cs
+++ NumberService/Program.cs
@@ -1,17 +1,13 @@
-using NumberService.Controllers;
-using System.Reflection.Metadata.Ecma335;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using NumberService.Logic;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-
+builder.Services.TryAddSingleton<Service>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
